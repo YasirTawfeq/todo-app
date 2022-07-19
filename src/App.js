43af2736a383369f08b3245adbaa6d0cc3780ef4
OKAMI3 <102405472+OKAMI3@users.noreply.github.com>
2022-07-19 11:30:53 +0300

    todo app

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import {useState} from 'react';
+import './App.css';
+
+
+function App() {
+  const[task,setTask]=useState([{id:234,text:'Add your Todo list'}])
+  const [input,setInput]=useState('');
+
+  const handleChange=(event)=>{
+    setInput(event.target.value)
+  }
+  const list={
+      id:Math.floor(Math.random() * 10000),
+      text:input
+    }
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    setTask(oldList=>[...oldList,list])
+    setInput('');
+  }
+
+  function delet(id){
+    const newItem=task.filter(item=>item.id!==id);
+    setTask(newItem);
+  }
+
+  
+  function edit(id){
+     task.find(item=>item.id===id?setInput(item.text= prompt('Edit')):'')
+     setInput('');
+  }
+
+  return (
+    <div className="App">
+      <div className="todo" >
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Task.." value={input} onChange={handleChange}/>
+        <button disabled={!input} type="submit">Add</button>
+      </form>
+      <ul>
+        {task?.map(task =>
+        <li >
+          {task.text}
+         <div> 
+            <button onClick={()=>edit(task.id)}>Edit</button>
+            <button onClick={()=>delet(task.id)}>Delete</button>
+         </div>
+        </li>)}
+      </ul>
+      </div>
+      
+    </div>
+  );
+}
+
+export default App;
